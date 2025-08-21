2383b153935617f0d94522d5b7ce4a46d39f6465
Adil-Bee200 <151982354+Adil-Bee200@users.noreply.github.com>
2025-08-21 10:53:39 +0500

    Created post service

---
+++ social_media_backend/src/main/java/com/example/social_media_backend/services/PostService.java
@@ -0,0 +1,149 @@
+package com.example.social_media_backend.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.social_media_backend.DTO.PostCreateDTO;
+import com.example.social_media_backend.DTO.PostResponseDTO;
+import com.example.social_media_backend.DTO.PostUpdateDTO;
+import com.example.social_media_backend.models.Post;
+import com.example.social_media_backend.repositories.PostRepository;
+
+@Service
+public class PostService {
+    private final PostRepository postRepository;
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+    
+    public List<PostResponseDTO> getAllPosts() {
+        List<Post> posts = postRepository.findAll();
+        List<PostResponseDTO> postResponses = new ArrayList<>();
+
+        for (Post post : posts) {
+            PostResponseDTO postResponse = convertToResponseDTO(post);
+            postResponses.add(postResponse);
+        }
+
+        return postResponses;
+    }
+
+    public List<PostResponseDTO> getPublishedPosts() {
+        List<Post> posts = postRepository.findByPublishedTrue();
+        List<PostResponseDTO> postResponses = new ArrayList<>();
+
+        for (Post post : posts) {
+            PostResponseDTO postResponse = convertToResponseDTO(post);
+            postResponses.add(postResponse);
+        }
+
+        return postResponses;
+    }
+
+    public List<PostResponseDTO> getPublishedPostsOrderByCreatedAt() {
+        List<Post> posts = postRepository.findAllPublishedPostsOrderByCreatedAtDesc();
+        List<PostResponseDTO> postResponses = new ArrayList<>();
+
+        for (Post post : posts) {
+            PostResponseDTO postResponse = convertToResponseDTO(post);
+            postResponses.add(postResponse);
+        }
+
+        return postResponses;
+    }
+
+    public PostResponseDTO getPostById(Long id) {
+        Optional<Post> post = postRepository.findById(id);
+
+        if (post.isPresent()) {
+            return convertToResponseDTO(post.get());
+        } else {
+            return null;
+        }
+    }
+
+    public List<PostResponseDTO> searchPostsByTitle(String title) {
+        List<Post> posts = postRepository.findByTitleContainingIgnoreCase(title);
+        List<PostResponseDTO> postResponses = new ArrayList<>();
+
+        for (Post post : posts) {
+            PostResponseDTO postResponse = convertToResponseDTO(post);
+            postResponses.add(postResponse);
+        }
+
+        return postResponses;
+    }
+
+    public PostResponseDTO createPost(PostCreateDTO postCreateDTO) {
+        Post newPost = new Post();
+        newPost.setTitle(postCreateDTO.getTitle());
+        newPost.setContent(postCreateDTO.getContent());
+        newPost.setPublished(postCreateDTO.getPublished());
+
+        Post createdPost = postRepository.save(newPost);
+
+        return convertToResponseDTO(createdPost);
+    }
+
+    public PostResponseDTO updatePost(Long id, PostUpdateDTO postUpdateDTO) {
+        Optional<Post> existingPost = postRepository.findById(id);
+
+        if (existingPost.isPresent()) {
+            Post post = existingPost.get();
+            
+            if (postUpdateDTO.getTitle() != null) {
+                post.setTitle(postUpdateDTO.getTitle());
+            }
+            if (postUpdateDTO.getContent() != null) {
+                post.setContent(postUpdateDTO.getContent());
+            }
+            if (postUpdateDTO.getPublished() != null) {
+                post.setPublished(postUpdateDTO.getPublished());
+            }
+
+            Post updatedPost = postRepository.save(post);
+
+            return convertToResponseDTO(updatedPost);
+        } else {
+            return null;
+        }
+    }
+
+    public boolean deletePost(Long id) {
+        Optional<Post> existingPost = postRepository.findById(id);
+
+        if (existingPost.isPresent()) {
+            postRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean togglePostPublishedStatus(Long id) {
+        Optional<Post> existingPost = postRepository.findById(id);
+
+        if (existingPost.isPresent()) {
+            Post post = existingPost.get();
+            post.setPublished(!post.getPublished());
+            postRepository.save(post);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private PostResponseDTO convertToResponseDTO(Post post) {
+        return new PostResponseDTO(
+            post.getId(),
+            post.getTitle(),
+            post.getContent(),
+            post.getPublished(),
+            post.getCreatedAt()
+        );
+    }
+} 
